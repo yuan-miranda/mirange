5c696f9d3f83236e7583618de3ba52b301cd43b4
yuan-miranda <yuanezekielamiranda@gmail.com>
2025-06-04 18:07:16 +0800

    added gridItemListener

--- static/js/control.js
+++ static/js/control.js
@@ -1,11 +1,14 @@
 import { loadHeader } from "./HEADER.js";
 
-document.addEventListener("DOMContentLoaded", async () => {
-    await loadHeader();
-
+function gridItemListener() {
     document.querySelectorAll('.grid-item').forEach(item => {
         item.addEventListener('touchstart', () => item.classList.add('pressed'));
         item.addEventListener('touchend', () => item.classList.remove('pressed'));
         item.addEventListener('touchcancel', () => item.classList.remove('pressed'));
     });
+}
+
+document.addEventListener("DOMContentLoaded", async () => {
+    await loadHeader();
+    gridItemListener();
 });
